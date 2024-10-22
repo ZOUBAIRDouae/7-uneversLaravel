2e4332b74c279239b1b5b961d56e9e5e39907fda
ZOUBAIRDouae <zoubairdouae5@gmail.com>
2024-10-22 16:41:38 +0100

    tuto2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/about', function () {
-    return view('about');
+// Route::get('/about', function () {
+//     return view('about');
+// });
+
+
+Route::get('/', function () {
+    return view('welcomeApp');
 });
